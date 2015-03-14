02f6188d50b5e0f14f714258237cfea9c369e34a
KuanYuLiu <ken5707@hotmail.com.tw>
2015-03-15 01:02:22 +0800

    how123

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -11,33 +11,38 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, lr}
+	push {r3,r4,r5,r6,r7,r8,r9,lr}
+	subs r4,r0,#0; @ R4 = R0 - 0 (update flags)
+	ble .L3 @ if(R0 <= 0) goto .L3 (which returns 0)
+	cmp r4, #1;  @ Compare R4 wtih 1
+	beq .L4 @ If R4 == 1 goto .L4 (which returns 1)
+        mov r6,#1
+	mov r7,#1
+	mov r9,#2
+	cmp r4,#1
+	bgt .Loop	
+	//pop {pc}		
+	@push {lr} 
+        @a0 =R6, a1=R7,fib=R8,x=R0 ,i=R9
+	
+.Loop:
+	add r8,r6,r7;  @fib = a0+a1 
+	mov r6,r7;       @a0=a1
+	mov r7,r8;       @a1=fib
+	add r9,r9,#1;  @i=i+1
+	cmp r9,r0;
+	blt .Loop
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
+	mov r0,r8
+	pop {r3,r4,r5,r6,r7,r8,r9,pc}
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
-
-	@ END CODE MODIFICATION
 .L3:
 	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
+	pop {pc}		@ EPILOG
+	@pop {pc}
 .L4:
 	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
-
+	pop {pc}		@ EPILOG
+	@pop {pc}
 	.size fibonacci, .-fibonacci
 	.end
